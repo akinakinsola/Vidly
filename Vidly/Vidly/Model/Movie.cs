5647443882039fd0026a93312af81c8fca0a039c
Akintomide Akinsola <akintomide.akinsola@fatman.fi>
2020-02-06 20:37:47 +0200

    added a new Movie Model

---
+++ Vidly/Vidly/Model/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Vidly.Models
+{
+	/// <summary>
+	/// Summary description for Movie
+	/// </summary>
+	///
+	public class Movie
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		
+	}
+
+}
